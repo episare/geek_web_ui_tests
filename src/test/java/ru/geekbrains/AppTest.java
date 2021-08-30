57de4bfd109336583121114a062dc6c94ccbcf55
episare <episare@rambler.ru>
2021-08-30 03:06:36 +0200

    Initial commit

---
+++ src/test/java/ru/geekbrains/AppTest.java
@@ -0,0 +1,26 @@
+package ru.geekbrains;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("AppTest1");
+    }
+
+    @Test
+    public void shouldAnswerWithTrue2()
+    {
+        System.out.println("AppTest2");
+    }
+}
---
+++ src/test/java/ru/geekbrains/IntegrationTest.java
@@ -0,0 +1,17 @@
+package ru.geekbrains;
+
+import org.junit.Test;
+
+public class IntegrationTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("Integration Test1");
+    }
+
+    @Test
+    public void shouldAnswerWithTrue2()
+    {
+        System.out.println("Integration Test2");
+    }
+}
